7763db4608543e9f4ff2a8a22da31cc0bb311ecc
root <you@example.com>
2024-11-26 20:35:06 +0000

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values,determine the
+fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Return: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if total <= 0:
+        return 0
+
+    current_total = 0
+    coin_used = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        r = (total-current_total)//coin
+        current_total += r*coin
+        coin_used += r
+        if current_total == total:
+            return coin_used
+    return -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+The task is to find the minimum number of coins needed to reach a specific total using a given list of coin denominations.
